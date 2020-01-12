71e9bd365b9c0ecdf271f9ba641b822d838dd6e2
carrymaniac <838537276@qq.com>
2020-01-12 21:36:51 +0800

    [update]修改了日志策略，测试成功

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 killall java
-nohup java -jar teaching-0.0.1-SNAPSHOT.jar --spring.profiles.active=pro > teaching.log 2>&1 &
+nohup java -jar teaching-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=pro > teaching.log 2>&1 &
 echo "START"
 
