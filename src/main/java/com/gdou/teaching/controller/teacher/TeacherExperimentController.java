08e6de5cb6cd1bf238eb31c1b50052a2ed5b5bc5
carrymaniac <838537276@qq.com>
2020-05-04 17:04:29 +0800

    refactor(TeacherExperimentController.java): 重构了@autowire的注入方式

--- src/main/java/com/gdou/teaching/controller/teacher/TeacherExperimentController.java
+++ src/main/java/com/gdou/teaching/controller/teacher/TeacherExperimentController.java
@@ -5,12 +5,11 @@ import com.gdou.teaching.Enum.ResultEnum;
 import com.gdou.teaching.dto.AnswerDTO;
 import com.gdou.teaching.dto.CourseDTO;
 import com.gdou.teaching.dto.ExperimentDTO;
+import com.gdou.teaching.event.EventProducer;
 import com.gdou.teaching.exception.TeachingException;
 import com.gdou.teaching.form.*;
-import com.gdou.teaching.service.AnswerService;
-import com.gdou.teaching.service.CourseService;
-import com.gdou.teaching.service.ExperimentService;
-import com.gdou.teaching.service.FileService;
+import com.gdou.teaching.mbg.model.AchievementExample;
+import com.gdou.teaching.service.*;
 import com.gdou.teaching.util.ResultVOUtil;
 import com.gdou.teaching.vo.CourseMainPageVO;
 import com.gdou.teaching.vo.ExperimentVO;
@@ -38,14 +37,22 @@ import java.util.stream.Collectors;
 @Slf4j
 @Auth
 public class TeacherExperimentController {
-    @Autowired
-    private ExperimentService experimentService;
-    @Autowired
-    private CourseService courseService;
-    @Autowired
-    private AnswerService answerService;
-    @Autowired
-    private FileService fileService;
+    private final ExperimentService experimentService;
+    private final CourseService courseService;
+    private final AnswerService answerService;
+    private final FileService fileService;
+    private final AchievementService achievementService;
+    private final EventProducer eventProducer;
+
+    public TeacherExperimentController(ExperimentService experimentService, CourseService courseService, AnswerService answerService, FileService fileService, AchievementService achievementService, EventProducer eventProducer) {
+        this.experimentService = experimentService;
+        this.courseService = courseService;
+        this.answerService = answerService;
+        this.fileService = fileService;
+        this.achievementService = achievementService;
+        this.eventProducer = eventProducer;
+    }
+
     /**
      * 实验列表
      * @param courseId
@@ -116,6 +123,12 @@ public class TeacherExperimentController {
         ExperimentDTO experimentDTO = new ExperimentDTO();
         BeanUtils.copyProperties(form, experimentDTO);
         experimentService.add(experimentDTO);
+        Integer courseId = experimentDTO.getCourseId();
+        List<Integer> userId = achievementService.getUserListByCourseId(courseId);
+        if(userId!=null&&!userId.isEmpty()){
+            //推送消息
+            userId.stream().forEach(id -> eventProducer.fireCourseUpdateEvent(id,courseId));
+        }
         return ResultVOUtil.success();
     }
     @PostMapping("/updateExperimentInfo")
